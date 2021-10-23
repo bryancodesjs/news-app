ba28dff707755e9aefc3c2bb69177b03795a788d
Bryan <72945681+bryancodesjs@users.noreply.github.com>
2021-10-23 18:19:51 -0400

    latest commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAT2zSG0ZkZsjS7BTp3dVyjTbRgewnLs1g",
+    authDomain: "alias-f4e8d.firebaseapp.com",
+    databaseURL: "https://alias-f4e8d-default-rtdb.firebaseio.com",
+    projectId: "alias-f4e8d",
+    storageBucket: "alias-f4e8d.appspot.com",
+    messagingSenderId: "380795112538",
+    appId: "1:380795112538:web:041c4095000c3d9a0a1c63"
+  }
 };
